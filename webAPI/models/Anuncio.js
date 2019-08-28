dca61a42b07efd1a2415e7224e36d21b10c27d95
Nacho <ignaciobarco@hotmail.com>
2019-08-28 18:58:24 +0200

    filtramos por nombre

--- webAPI/models/Anuncio.js
+++ webAPI/models/Anuncio.js
@@ -7,12 +7,21 @@ const anunciosSchema = mongoose.Schema({
     nombre: String,
     venta: Boolean,
     precio: String,
-    foto: String
-    //,
-    //tags: [String]
+    foto: String,
+    tags: [String]
 }
     //, { collection: 'agentes'} // para saltarse la pluralización
 );
+
+anunciosSchema.statics.list = function({filter}){ //, skip, limit, fields, sort}) {
+    const query = Anuncio.find(filter);
+    // query.skip(skip);
+    // query.limit(limit);
+    // query.select(fields);
+    // query.sort(sort);
+    return query.exec();
+  }
+
 
 // creamos el modelo de agente
 const Anuncio = mongoose.model('Anuncio', anunciosSchema);
--- webAPI/routes/apiv1/anuncios.js
+++ webAPI/routes/apiv1/anuncios.js
@@ -18,6 +18,27 @@ router.get('/prueba/id/:id', (req, res, next) => {
     console.log('req.params', req.params);
     console.log('prueba');
     res.send('ok');
-  });
+});
+
+router.get('/prueba2', async (req, res, next) => {
+    try {
+        const nombre = req.query.nombre;
+
+        const filter = {};
+
+        if (nombre) {
+            filter.nombre = nombre;
+        }
+
+        const anuncios = await Anuncio.list({ filter: filter });
+
+        res.json({ success: true, anuncios: anuncios });
+
+    } catch (err) {
+        next(err);
+    }
+
+
+});
 
 module.exports = router;
