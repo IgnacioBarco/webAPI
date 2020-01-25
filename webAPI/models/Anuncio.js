01f6deb95ce56e43bd34e538d10539ee80d2bd4d
nacho <ignaciobarco@hotmail.com>
2020-01-25 13:54:12 +0100

    añadimos la devolucion de tags por la api

--- webAPI/models/Anuncio.js
+++ webAPI/models/Anuncio.js
@@ -1,29 +1,33 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // definimos un esquema
-const anunciosSchema = mongoose.Schema({
+const anunciosSchema = mongoose.Schema(
+  {
     nombre: String,
     venta: Boolean,
     precio: Number,
     foto: String,
     tags: [String]
-}
-    //, { collection: 'agentes'} // para saltarse la pluralización
+  }
+  //, { collection: 'agentes'} // para saltarse la pluralización
 );
 
-anunciosSchema.statics.list = function({filter, skip, limit, fields, sort}) {
-    const query = Anuncio.find(filter);
-    query.skip(skip);
-    query.limit(limit);
-    query.select(fields);
-    query.sort(sort);
-    return query.exec();
-  }
+anunciosSchema.statics.list = function({ filter, skip, limit, fields, sort }) {
+  const query = Anuncio.find(filter);
+  query.skip(skip);
+  query.limit(limit);
+  query.select(fields);
+  query.sort(sort);
+  return query.exec();
+};
 
+anunciosSchema.statics.tags = function() {
+  return ["work", "lifestyle", "motor", "mobile"];
+};
 
 // creamos el modelo de agente
-const Anuncio = mongoose.model('Anuncio', anunciosSchema);
+const Anuncio = mongoose.model("Anuncio", anunciosSchema);
 
 module.exports = Anuncio;
--- webAPI/routes/apiv1/anuncios.js
+++ webAPI/routes/apiv1/anuncios.js
@@ -1,66 +1,78 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Anuncio = require('../../models/Anuncio');
+const Anuncio = require("../../models/Anuncio");
 
-router.get('/', async (req, res, next) => {
-    try {
-        const anuncios = await Anuncio.find().exec();
-        res.json({ success: true, anuncios: anuncios });
-        
-    } catch (err) {
-        next(err);
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    const anuncios = await Anuncio.find().exec();
+    res.json({ success: true, anuncios: anuncios });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/prueba/id/:id', (req, res, next) => {
-    console.log('req.params', req.params);
-    console.log('prueba');
-    res.send('ok');
+router.get("/tags", (req, res, next) => {
+  try {
+    const anuncios = Anuncio.tags();
+    // res.json({ success: true, anuncios: anuncios });
+    res.json(anuncios);
+    res.send("ok");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/prueba2', async (req, res, next) => {
-    try {
-        const nombre = req.query.nombre;
-        const venta = req.query.venta;
-        const tags = req.query.tags;
-        const precio = req.query.precio;
+router.get("/prueba/id/:id", (req, res, next) => {
+  console.log("req.params", req.params);
+  console.log("prueba");
+  res.send("ok");
+});
 
-        const skip = parseInt(req.query.skip);
-        const limit = parseInt(req.query.limit);
-        const fields = req.query.fields;
-        const sort = req.query.sort;
+router.get("/prueba2", async (req, res, next) => {
+  try {
+    const nombre = req.query.nombre;
+    const venta = req.query.venta;
+    const tags = req.query.tags;
+    const precio = req.query.precio;
 
+    const skip = parseInt(req.query.skip);
+    const limit = parseInt(req.query.limit);
+    const fields = req.query.fields;
+    const sort = req.query.sort;
 
-        const filter = {};
+    const filter = {};
 
-        if (nombre) {
-            filter.nombre = new RegExp(req.query.nombre, "i");
-        }
-
-        if (venta) {
-            filter.venta = venta;
-        }
-
-        if (tags) {
-            filter.tags = tags;
-        }
-
-        if (typeof precio !== 'undefined') {
-            filter.precio = precio;
-        }
-
-        const anuncios = await Anuncio.list({ filter: filter, skip, limit, fields, sort });
-
-        res.json({ success: true, anuncios: anuncios });
-
-    } catch (err) {
-        next(err);
+    if (nombre) {
+      filter.nombre = new RegExp(req.query.nombre, "i");
     }
 
+    if (venta) {
+      filter.venta = venta;
+    }
 
+    if (tags) {
+      filter.tags = tags;
+    }
+
+    if (typeof precio !== "undefined") {
+      filter.precio = precio;
+    }
+
+    const anuncios = await Anuncio.list({
+      filter: filter,
+      skip,
+      limit,
+      fields,
+      sort
+    });
+
+    res.json({ success: true, anuncios: anuncios });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
